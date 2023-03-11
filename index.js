9c08dd230a14f93868499bcc468cefd8a164782e
unknown <pavanrajesh365@gmail.com>
2023-03-11 23:48:12 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const body = document.getElementsByTagName("body")[0];
+let previousX = 0;
+let previousY = 0;
+const dotAppender = (e) => {
+  const clientX = e.clientX;
+  const clientY = e.clientY;
+  const dot = document.createElement("div");
+  dot.classList.add("dot");
+  dot.style.left = `${clientX}px`;
+  dot.style.top = `${clientY}px`;
+  body.appendChild(dot);
+  drawLine(clientX, clientY);
+};
+body.addEventListener("click", dotAppender);
+
+const drawLine = (x1, y1) => {
+  const line = document.createElement("div");
+  line.classList.add("line");
+  line.style.left = `${x1}px`;
+  line.style.top = `${y1}px`;
+
+  const lineHeight = Math.sqrt(
+    Math.pow(x1 - previousX, 2) + Math.pow(y1 - previousY, 2)
+  );
+  line.style.height = `${lineHeight}px`;
+  console.log(x1, y1, previousX, previousY);
+  if (x1 < previousX) {
+    line.style.left = `${previousX}px`;
+    line.style.top = `${previousY}px`;
+  }
+  const angleRadian = Math.atan((y1 - previousY) / (x1 - previousX));
+  const angleDegree = (angleRadian * 180) / Math.PI;
+  console.log(angleDegree);
+
+  line.style.transform = `rotate(${angleDegree + 90}deg)`;
+
+  body.appendChild(line);
+  previousX = x1;
+  previousY = y1;
+};
+
+const undoButton = document.getElementById("undo");
+undoButton.addEventListener("click", removeLine);
+
+function removeLine(e) {
+  e.stopPropagation();
+
+  body.lastChild.remove();
+  body.lastChild.remove();
+  const lastLine = body.lastChild;
+  previousX = parseInt(lastLine.style.left);
+  previousY = parseInt(lastLine.style.top);
+}
